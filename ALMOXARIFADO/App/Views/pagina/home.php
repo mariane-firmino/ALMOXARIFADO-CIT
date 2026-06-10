221e00d6d664b0303fbcc58b83ad79f07a49ffb2
Mariane Firmino <marianecfm2024@gmail.com>
2026-06-10 17:29:37 -0400

    Versão nova
    Arrumando as páginas em um novo repositório.

---
+++ ALMOXARIFADO/App/Views/pagina/home.php
@@ -0,0 +1,44 @@
+<form action="" method="post">
+ <!-- Login Container -->
+    <div class="login-container">
+        <!-- Envolve as duas partes do login (branco e verde)-->
+        <div class="login-wrapper">
+            <!-- Lado esquerdo -->
+            <div class="login-left">
+                <h1 class="login-title">Login</h1>
+
+                <div class="form-group">
+                    <label class="form-label">Usuário</label>
+                    <input type="text" name="usuario" class="form-input" placeholder="">
+                </div>
+
+                <div class="form-group">
+                    <label class="form-label">Senha</label>
+                    <input type="password" name="senha" class="form-input" placeholder="">
+                </div>
+
+                <button class="submit-btn">
+                    <input type="submit" class="submit-text" value="Enviar">
+                </button>
+
+                <div class="footer-links">
+                    <div class="link-row">
+                        <span class="link-text">Esqueceu a senha?</span>
+                        <a href="esqueciSenha.html" class="link-blue">Esqueci minha senha</a>
+                    </div>
+                    <div class="link-row">
+                        <span class="link-text">Ainda não é cadastrado?</span>
+                        <a href="cadastro.html" class="link-blue">Cadastre-se</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Lado direito - Logo -->
+            <div class="login-right">
+                <div class="logo-container">
+                    <img alt="SACIT Logo" class="logo-image" src="img/logo-sacit.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
